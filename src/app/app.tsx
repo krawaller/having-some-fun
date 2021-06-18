cc047679e3ffd2c6cc65f706ebcb170c226c7aed
David <david@krawaller.se>
2021-06-18 16:30:49 +0200

    extract layout component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,49 +1,35 @@
 import { useAtom } from 'klyva'
-import styled from 'styled-components'
 import React, { useMemo } from 'react'
 import { Catalog, Meta } from '../data/types'
 import { makeInventory } from '../state/inventory'
 import { Pantry } from './pantry'
 import { List } from './list'
 import { Summary } from './summary'
+import { Layout } from './layout'
 
 type AppProps<C extends Catalog> = {
   catalog: C
   meta: Meta<C>
 }
 
-const Container = styled.div`
-  display: flex;
-`
-
-const Left = styled.div`
-  flex-grow: 1;
-`
-
-const Right = styled.div`
-  width: 400px;
-  flex-shrink: 0;
-`
-
 export const App = <C extends Catalog>(props: AppProps<C>) => {
   const { catalog, meta } = props
   const { actions, inventory } = useAppState(catalog)
-  return (
-    <Container>
-      <Left>
-        <Pantry actions={actions} catalog={catalog} meta={meta} />
-        <List actions={actions} state={inventory} meta={meta} />
-      </Left>
-      <Right>
-        <Summary
-          actions={actions}
-          state={inventory}
-          catalog={catalog}
-          meta={meta}
-        />
-      </Right>
-    </Container>
+  const main = (
+    <>
+      <Pantry actions={actions} catalog={catalog} meta={meta} />
+      <List actions={actions} state={inventory} meta={meta} />
+    </>
   )
+  const side = (
+    <Summary
+      actions={actions}
+      state={inventory}
+      catalog={catalog}
+      meta={meta}
+    />
+  )
+  return <Layout main={main} side={side} />
 }
 
 const useAppState = (catalog: Catalog) => {
---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import React, { ReactElement } from 'react'
+import styled from 'styled-components'
+
+type LayoutProps = {
+  main: ReactElement
+  side: ReactElement
+}
+
+export const Layout = (props: LayoutProps) => {
+  const { main, side } = props
+  return (
+    <Container>
+      <Main>{main}</Main>
+      <Side>{side}</Side>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+`
+
+const Main = styled.div`
+  flex-grow: 1;
+`
+
+const Side = styled.div`
+  width: 400px;
+  flex-shrink: 0;
+`
